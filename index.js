640f4ae145db6b47b472c87581771d9951614fab
Radiant <you@example.com>
2020-09-10 23:22:24 +0700

    nambah file

---
+++ index.js
@@ -0,0 +1,8 @@
+var santripondok = ['Aandi','Aldi','Acep','Putra','Hafif','Syofyan','Rahmad']
+document.write('<ol>')
+for(i = 0; i < santripondok.length; i++) {
+    document.write('<li>')
+    document.write(santripondok[i]);
+    document.write('</li>')
+}
+document.write('</ol>')
